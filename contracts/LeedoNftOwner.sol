1c19e68d972968a82d96c389f16deab5895c0a28
GoSquidGame <GoSquidGame@gmail.com>
2021-12-03 15:37:05 -0600

    updated NftOwner

--- contracts/LeedoNftOwner.sol
+++ contracts/LeedoNftOwner.sol
@@ -16,48 +16,6 @@ pragma solidity ^0.8.0;
 
 interface INft {
 
-    function ownerOf(uint256 tokenId) external view returns (address owner);
-}
-
-contract NftOwner {
-    
-    address public nftAddr;
-    address public nftVaultAddr;
-
-    constructor(address _nftAddr, address _nftVaultAddr) {
-        nftAddr = _nftAddr;
-        nftVaultAddr = _nftVaultAddr;
-    }
-    
-    function ownerOf(uint256 tokenId) external view returns (address) {
-        INft NFT = INft(nftAddr);
-        if (NFT.ownerOf(tokenId) == nftVaultAddr) {
-            INft Vault = INft(nftVaultAddr);
-            return Vault.ownerOf(tokenId);
-        } else {
-            return NFT.ownerOf(tokenId);
-        }
-    }
-}
-
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-/**
- * @dev LEEDO NFT Ownership Proxy
- *
- *  _              ______      
- * | |             |  _  \     
- * | |     ___  ___| | | |___  
- * | |    / _ \/ _ \ | | / _ \ 
- * | |___|  __/  __/ |/ / (_) |
- * \_____/\___|\___|___/ \___/ 
- * LEEDO Project
- */
-
-interface INft {
-
     function ownerOf(uint tokenId) external view returns (address owner);
     function tokensOf(address account) external view returns (uint[] memory);
     function balanceOf(address account) external view returns (uint256); 
